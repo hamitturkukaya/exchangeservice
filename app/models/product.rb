e94ce32bfd2a7b879c2d55d7414181bbd0e33f3d
hamitturkukaya <htkaya90@gmail.com>
2013-03-01 15:24:48 +0200

    heroku

--- app/models/product.rb
+++
@@ -1,4 +0,0 @@
-class Product < ActiveRecord::Base
-  attr_accessible :price, :product
-  monetize :price_cents
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Exchangeservice::Application.routes.draw do
-  resources :products
-
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
